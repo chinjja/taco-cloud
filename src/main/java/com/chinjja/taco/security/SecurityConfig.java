61daf865c650216cd29afd2f27036afd48a5571c
chinjja <chinjja@gmail.com>
2021-04-30 18:50:10 +0900

    security

--- src/main/java/com/chinjja/taco/security/SecurityConfig.java
+++ src/main/java/com/chinjja/taco/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.chinjja.taco.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,7 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -22,14 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("user1")
-		.password("{noop}password1")
-		.authorities("ROLE_USER")
-		.and()
-		.withUser("user2")
-		.password("{noop}password2")
-		.authorities("ROLE_USER");
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select username, authority from authorities where username=?");
 	}
 
 }
