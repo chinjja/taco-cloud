4a50a9d94eb12accf05353bb31bf5d68b74d997c
chinjja <chinjja@gmail.com>
2021-05-13 17:26:29 +0900

    @Validated to OrderProps

--- src/main/java/com/chinjja/taco/web/OrderProps.java
+++ src/main/java/com/chinjja/taco/web/OrderProps.java
@@ -1,13 +1,20 @@
 package com.chinjja.taco.web;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
 
 @Component
 @ConfigurationProperties("taco.orders")
 @Data
+@Validated
 public class OrderProps {
+	@Min(5)
+	@Max(25)
 	private int pageSize = 20;
 }
